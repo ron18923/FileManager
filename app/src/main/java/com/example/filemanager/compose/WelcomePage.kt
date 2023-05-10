2dbdc5c3233e12d122c689720be30b2cf682ccb6
ron18923 <ronron18923@gmail.com>
2023-05-10 19:26:10 +0300

    base of the project. The project will be a file manager demo

---
+++ app/src/main/java/com/example/filemanager/compose/WelcomePage.kt
@@ -0,0 +1,15 @@
+package com.example.filemanager.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WelcomePage(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomePagePrev(){
+    WelcomePage()
+}
---
+++ app/src/main/java/com/example/filemanager/ui/theme/Theme.kt
@@ -0,0 +1,50 @@
+package com.example.filemanager.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+
+    background = Color(0xffBDE4F4)
+
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+
+    /* Other default colors to override
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun FileManagerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+
+    val colors = LightColorPalette
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
